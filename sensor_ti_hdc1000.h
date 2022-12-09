1bcd917f08023562b8832e793ccb7ce0202ae0a4
zhkag <zhkag@foxmail.com>
2022-12-09 11:15:17 +0800

    适配 5.0.0 改动

--- sensor_ti_hdc1000.h
+++ sensor_ti_hdc1000.h
@@ -2,7 +2,13 @@
 #ifndef SENSOR_TI_HDC1000_H__
 #define SENSOR_TI_HDC1000_H__
 
+#include <rtthread.h>
+#if defined(RTTHREAD_VERSION) && (RTTHREAD_VERSION >= RT_VERSION_CHECK(5, 0, 0))
+#include "drivers/sensor.h"
+#else
 #include "sensor.h"
+#endif
+
 #include "hdc1000_reg.h"
 
 #define HDC1000_SLAVE_ADDR_DEFAULT       ( HDC1000_DEVICE_IIC_BUS_ADDRESS )
